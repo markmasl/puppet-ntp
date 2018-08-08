f3cb0cd760ac2683e6eb14b4bf54837f04e3e895
root <root@neotech3.mylabserver.com>
2018-08-08 20:55:13 +0000

    fix

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,7 @@ class ntp::service (
   String $service_ensure      = $ntp::service_ensure,
   Boolean $service_enable     = $ntp::service_enable,
   Boolean $service_hasstatus  = $ntp::service_hasstatus,
-  Boolean $service_hasrestart = $ntp::servicehasrestart,
+  Boolean $service_hasrestart = $ntp::service_hasrestart,
 ) {
   service { 'ntp':
     ensure     => $service_ensure,
